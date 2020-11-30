f0667b66aeefa8e69bf51acb25f79d96f4e8b7d3
Andrea <dauriaandrea99@gmail.com>
2020-11-30 18:05:40 +0100

    Dropdown Funzionante ! END !

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,10 @@
 $(function() {
     
-$(".dropdown").hover(function () { 
+$(".dropdown > a").mouseenter(function () { 
     $(".dropdown-menu").removeClass("d-block");
     $(this).next(".dropdown-menu").addClass("d-block");
 });
+
 $(".dropdown").mouseleave(function () { 
     $(this).next(".dropdown-menu").addClass("d-block");
     $(".dropdown-menu").removeClass("d-block");
