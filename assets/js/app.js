eb0fb037c5ef1e96e90ea8b6a9fb5ee280b1f840
Andrea <dauriaandrea99@gmail.com>
2020-11-30 17:45:01 +0100

    Drop funzioante a metà

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+$(function() {
+    
+$(".dropdown").hover(function () { 
+    $(".dropdown-menu").removeClass("d-block");
+    $(this).next(".dropdown-menu").addClass("d-block");
+});
+$(".dropdown").mouseleave(function () { 
+    $(this).next(".dropdown-menu").addClass("d-block");
+    $(".dropdown-menu").removeClass("d-block");
+});
+
+
+});
